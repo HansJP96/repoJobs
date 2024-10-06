2670396e5869aa53c3f4fdbae9072b0dfbfe5818
Hansee Jimenez <hansee1996@gmail.com>
2024-10-06 11:01:59 -0500

    trigger problem

--- CursoDevops/Billing/Dev/DockerImagePublish/PipelineConfig.groovy
+++ CursoDevops/Billing/Dev/DockerImagePublish/PipelineConfig.groovy
@@ -2,6 +2,24 @@
 pipelineJob("DIRECTORY_PATH/PublishDockerHub") {
     description("Pipeline configurado dinámicamente")
     
+    properties {
+		pipelineTriggers {
+			triggers {
+				githubPush() // Trigger para GitHub Push (cuando se haga un push en el repositorio)
+			}
+    	}
+        buildDiscarder {
+            strategy {
+                logRotator {
+                    numToKeepStr('10')   
+                    daysToKeepStr('7')  
+                    artifactNumToKeepStr('7') 
+                    artifactDaysToKeepStr('7') 
+                }
+            }
+        }
+    }
+    
     // Configurar el SCM para que apunte a tu repositorio
     definition {
         cpsScm {
@@ -18,27 +36,6 @@ pipelineJob("DIRECTORY_PATH/PublishDockerHub") {
 		cps {
             script(readFileFromWorkspace(SEED_JOB.lastBuild.checkouts[0].workspace + "/" + "JENKINSFILE_PATH"))
             sandbox()
-        }
-    }
-    
-    // Triggers que activarán el pipeline
-    pipelineTriggers {
-		triggers {
-			githubPush() // Trigger para GitHub Push (cuando se haga un push en el repositorio)
-		}
-    }
-	
-	
-	properties {
-        buildDiscarder {
-            strategy {
-                logRotator {
-                    numToKeepStr('10')   
-                    daysToKeepStr('7')  
-                    artifactNumToKeepStr('7') 
-                    artifactDaysToKeepStr('7') 
-                }
-            }
         }
     }
 }
